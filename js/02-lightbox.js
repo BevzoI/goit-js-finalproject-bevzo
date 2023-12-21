7cab273162f342c311e42eae6ffcb01887e25d07
BevzoI <bevzo.i.n@gmail.com>
2023-12-21 19:29:09 +0100

    add js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryRef = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ description, preview, original }) => `
+   <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`
+  )
+  .join("");
 
-console.log(galleryItems);
+if (!galleryRef) console.error("Gallery not found");
+galleryRef.insertAdjacentHTML("afterbegin", markup);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 500,
+});
